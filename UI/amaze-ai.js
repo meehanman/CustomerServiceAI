14f84971143de3382b230fd11b4733bf8d77091d
Dean Meehan <d3an.meehan@hotmail.com>
2018-03-25 02:39:02 +0100

    wesfcs

--- UI/amaze-ai.js
+++ UI/amaze-ai.js
@@ -1,229 +1,76 @@
  // HTTP object to make REST requests
- var xhr = new XMLHttpRequest();
- var gotProblems = false;
- var gotResolutions = false;
- var msg = '';
+ var botName = "Chatbot"
  var user;
  var csr;
- var custData = {
-    "_id": 234324342,
-    "name": "Dean Meehan",
-    "email": "d3an.meehan@hotmail.com",
-    "order": [{
-        "_id": 2423423,
-        "product": "Sony Vio Laptop 2006",
-        "description": "laptop",
-        "reviews": ["", "", "", "", "", "", "", "", "", "", ""]
-      },
-      {
-        "_id": 2423423,
-        "product": "Samsumg QLED 2017 AE3000B32",
-        "description": "tv cable screen stand off",
-        "reviews": ["", "", "", "", "", "", "", "", "", "", ""]
-      },
-      {
-        "_id": 2423423,
-        "product": "Apple Watch 2018",
-        "description": "watch battery strap",
-        "reviews": ["", "", "", "", "", "", "", "", "", "", ""]
-      }
-    ]
-  };
- var locationIndex = 0;
 
+ function displayRecentOrders(arrOfOrderNames) {
+   var displayProblemsToUser = '';
+   displayProblemsToUser += '<div class="list-group"><div class="list-group-item list-group-item-action active"><b class="subtext"> Automatic Assistant </b>';
+   displayProblemsToUser += '</br>' + "Oops, We couldent find your order...heres a list of your orders please selct one of the below:" + '</div>';
+   for (var i = 0; i < arrOfOrderNames.length; i++) {
+     displayProblemsToUser += '<a onclick="getResolutions()" href="#" class="list-group-item list-group-item-action">' + custData.order[i].product + '</a>';
+   }
+   displayProblemsToUser += "</div>";
+   console.log("displayProblemsToUser", displayProblemsToUser);
+   $("#chatWindowText").append(displayProblemsToUser);
+   $("#userInput").val('');
+ }
 
+ function getResponce(question) {
+   $("#userInput").val('');
 
- $.ajax({
-    url: 'https://randomuser.me/api/?nat=gb',
-    dataType: 'json',
-    success: function(data) {
-        user = data.results[0];
-        loaded();
-    }
-  });
+   if(contains(question,"laptop")){
+    writeMessage(botName, "We couldn't find any suggested problems with your laptop. Connecting you to an agent...");
+    //Display Laptop Reviews
+    return;
+   }
 
-  $.ajax({
-    url: 'https://randomuser.me/api/?nat=IE',
-    dataType: 'json',
-    success: function(data) {
-        csr = data.results[0];
-        loaded();
-    }
-  });
+   if(contains(question,"order")){
+    writeMessage(botName, "Your recent orders were a Samsung TV, Apple Watch, Kettle and 3+ items. Do you need help with any of these?", "order");
+    //Display Orders
+    return;
+   }
 
-  function update(){
-    $("[data-csr='user.profile.image").attr("src",csr.picture.large);
-    $("[data-csr='user.profile.name").text(csr.name.first+" "+user.name.last);
-    $("[data-csr='user.profile.firstname").text(csr.name.first);
-    $("[data-hero='user.profile.image").attr("src",user.picture.large);
-    $("[data-hero='user.profile.name").text(user.name.first+" "+user.name.last);
-    $("[data-hero='user.profile.firstname").text(user.name.first);
+   if(contains(question,"watch")||contains(question,"apple")){
+    writeMessage(botName, "Hey, what problems did you have with your order?", "watch");
+    //Display Orders
+    return;
+   }
 
+   if(contains(question,"tv")){
+    writeMessage(botName, "Did you have a problem with your TV not turning on?", "tv");
+    //Display Orders
+    return;
+   }
+
+   if(contains(question,"yes")){
+    writeMessage(botName, "Could not Connect to Agent. Please try again later. [Error 500]");
+    //Display Orders
+    return;
+   }
+
+   if(contains(question,"tv") && contains(question,"")){
+    writeMessage(botName, "Could not Connect to Agent. Please try again later. [Error 500]");
+    //Display Orders
+    return;
+   }
+   
+   //Conversational
+   if (locationIndex == 0) {
+    writeMessage(botName, "Hey <span data-hero='user.profile.firstname'/>, how can we help today?");
+    locationIndex++;
+  } else if (locationIndex == 1) {
+    writeMessage(botName, "Ooh, can you try that again?");
+    locationIndex++;
+  }else if (locationIndex == 2) {
+    writeMessage(botName, "Sorry, I didn't get that. Do you want to talk to a representative?");
+    locationIndex++;
+  }else if (locationIndex == 3) {
+    writeMessage(botName, "Closing Chat.");
+    $('#userInput').attr("disabled","disabled");
+    locationIndex++;
   }
-
-  function loaded(){
-    if(csr!=null && user!=null){
-        $('#loading').hide();
-        $('#main-content').show();
-        update();
-        return true;
-    }
-    return false;
-  }
-
- function addChatBot(user, msg) {
-     if(user != "You"){
-        locationIndex++;
-     }
- 	$("#chatWindowText")
-         .append('<nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item active" aria-current="page"><b class="subtext">' + user + '</b></br>' + msg + '</li></ol></nav>');
-         if(loaded()){
-            update();
-         }
  };
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function displayRecentOrders(){
-    var displayProblemsToUser = '';
-    displayProblemsToUser += '<div class="list-group"><div class="list-group-item list-group-item-action active"><b class="subtext"> Automatic Assistant </b>';
-    displayProblemsToUser += '</br>' + "Oops, We couldent find your order...heres a list of your orders please selct one of the below:" + '</div>';
-    for (var i = 0; i < custData.order.length; i++) {
-        displayProblemsToUser += '<a onclick="getResolutions()" href="#" class="list-group-item list-group-item-action">' + custData.order[i].product + '</a>';
-    }
-    displayProblemsToUser += "</div>";
-    console.log("displayProblemsToUser", displayProblemsToUser);
-    $("#chatWindowText").append(displayProblemsToUser);
-    $("#userInput").val('');
-}
-
-$('#submitQuestion').click(function(){
-    start();
-});
-
-$('#userInput').on('keypress', function (e) {
-    if(e.which === 13){
-    start($('#userInput').val());  
-    $('#userInput').val("");
-    
-    }
-});
-
-function start(aaaaaa){
-    setTimeout(function(){ 
-        var input = aaaaaa;
-        if(input != ""){
-            addChatBot("You", input);
-            setTimeout(function(){
-            getProblems(input)
-            });
-        }
-    }, 500);
-}
-
-function checkRecentOrders(question){
-     	//check if the user has asked for an item they bought
- 	//is any of the text in the accepted custData.order
- 	var validOrder = false;
- 	for (var i = 0; i < custData.order.length; i++) {
- 		if (question.indexOf(custData.order[i].product) !== -1) {
- 			validOrder = true;
- 			break;
- 		}
- 	}
- 	if (!validOrder) {
-        displayRecentOrders();
-     }
-}
-
- function getProblems(question) {
-    console.info("State",locationIndex);
-
-     if(locationIndex == 0){
-        addChatBot("Chat Bot", "Hey <span data-hero='user.profile.firstname'/>, how can we help today?");
-     }else if(locationIndex == 1){
-        addChatBot("Chat Bot", "Hey <span data-hero='user.profile.firstname'/>, how can we help today?");
-     }
-     
-     //Clear
- 	$("#userInput").val('');
- };
- getProblems();
-
- function _getProblemsFromServer(){
-    var request = "http://167.99.91.119:8080/getProblems";
-    console.log(request);
-    xhr.open('GET', request, true);
-    xhr.onreadystatechange = getProblemsServerResponse;
-    xhr.onerror = function () {
-        serverResponse.innerHTML = "ERROR: An error occurred during the HTTP Request";
-    };
-    xhr.send();
- }
-
- ///After gotProblems
- function getResolutions() {
- 	addChatBot("You", "Thanks, the top reported issues for your selected item are below, do any of them match your issue?");
- 	$("#userInput")
- 		.val('');
- 	var request = "http://167.99.91.119:8080/getResolutions";
- 	gotProblems = true;
- 	console.log(request);
- 	xhr.open('GET', request, true);
- 	xhr.onreadystatechange = getResolutionsServerResponse;
- 	xhr.onerror = function () {
- 		serverResponse.innerHTML = "ERROR: An error occurred during the HTTP Request";
- 	};
- 	xhr.send();
- }
-
- function getProblemsServerResponse(e) {
- 	//We should have a switch statment to handle multiple responses
- 	//In the future we can use these singals to monitor performance
- 	console.log("Processing Request status: ", xhr.readyState, " - ", xhr.status);
- 	if (xhr.readyState === 4) {
- 		if (xhr.status == 404) {
- 			console.log(xhr.responseText);
- 		}
- 		if (xhr.status == 200) {
- 			var result = JSON.parse(xhr.responseText);
- 			console.log(result);
- 			var displayProblemsToUser = '';
- 			displayProblemsToUser += '<div class="list-group"><div class="list-group-item list-group-item-action active"><b class="subtext"> Automatic Assistant </b>';
- 			displayProblemsToUser += '</br>' + "Here is a list of the most common problems, please select one:" + '</div>';
- 			for (var i = 0; i < result.length; i++) {
- 				console.log(result[i].term);
- 				displayProblemsToUser += '<a onclick="getResolutions()" href="#" class="list-group-item list-group-item-action">' + result[i].term + '</a>';
- 			}
- 			displayProblemsToUser += "</div>";
- 			console.log("displayProblemsToUser", displayProblemsToUser);
- 			$("#chatWindowText")
- 				.append(displayProblemsToUser);
- 		}
- 	}
- }
-
- function getResolutionsServerResponse(e) {
- 	//We should have a switch statment to handle multiple responses
- 	//In the future we can use these singals to monitor performance
- 	console.log("Processing Request status: ", xhr.readyState, " - ", xhr.status);
- 	if (xhr.readyState === 4) {
- 		if (xhr.status == 404) {
- 			console.log(xhr.responseText);
- 		}
- 		if (xhr.status == 200) {
- 			var result = JSON.parse(xhr.responseText);
- 			console.log(result);
- 			var displayProblemsToUser = '';
- 			displayProblemsToUser += '<div class="list-group"><div class="list-group-item list-group-item-action active"><b class="subtext"> Automatic Assistant </b>';
- 			displayProblemsToUser += '</br>' + "Heres a list of the most common resolutions, please select one:" + '</div>';
- 			for (var i = 0; i < result.length; i++) {
- 				console.log(result[i].term);
- 				displayProblemsToUser += '<a onclick="getResolutions()" href="#" class="list-group-item list-group-item-action">' + result[i].term + '</a>';
- 			}
- 			displayProblemsToUser += "</div>";
- 			console.log("displayProblemsToUser", displayProblemsToUser);
- 			$("#chatWindowText")
- 				.append(displayProblemsToUser);
- 		}
- 	}
- }
+ //Load first one
+ getResponce();
---
+++ UI/finishedCode.js
@@ -0,0 +1,106 @@
+ //Called by the Send button
+ function start(input) {
+    if (input != "") {
+     console.log(input, input != "",input == "");
+      writeMessage("You", input);
+      getResponce(input);
+    }
+  }
+
+  function writeMessage(user, msg, type) {
+    var delay;
+    //BOT
+    if (user == botName) {
+      delay = 1000;
+      //NOT BOT
+    } else {
+      delay = 0;
+    }
+ 
+    setTimeout(() => {
+        if(type==undefined){
+            $("#chatWindowText").append('<nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item active" aria-current="page"><b class="subtext">' + user + '</b></br>' + msg + '</li></ol></nav>');
+        }else if(type=="order"){
+            $("#chatWindowText").append('<nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item active" aria-current="page"><b class="subtext">' + user + '</b></br>' + msg + '</li></ol></nav>');
+        }else if(type=="watch"){
+            $("#chatWindowText").append('<nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item active" aria-current="page"><b class="subtext">' + user + '</b></br>' + msg + '</li></ol></nav>'+getItemHTML(2));
+        }else if(type=="tv"){
+            $("#chatWindowText").append('<nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item active" aria-current="page"><b class="subtext">' + user + '</b></br>' + msg + '</li></ol></nav>'+getItemHTML(1));
+        }else if(type=="laptop"){
+            $("#chatWindowText").append('<nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item active" aria-current="page"><b class="subtext">' + user + '</b></br>' + msg + '</li></ol></nav>'+getItemHTML(0));
+        }
+      if (loaded()) {
+        update();
+      }
+      scrollDivToBottom();
+    }, delay);
+  };
+
+  function getItemHTML(index){
+      console.log(index);
+return '<div class="card" style="width: 18rem;"> <img class="card-img-top" src="'+custData.order[index].image+'" alt="Card image cap"> <div class="card-body"> <h5 class="card-title">'+custData.order[index].product+'</h5></p> </div> </div>'
+  }
+   //Used to update the names and images
+ function update() {
+    $("[data-csr='user.profile.image").attr("src", csr.picture.large);
+    $("[data-csr='user.profile.name").text(csr.name.first + " " + user.name.last);
+    $("[data-csr='user.profile.firstname").text(csr.name.first);
+    $("[data-hero='user.profile.image").attr("src", user.picture.large);
+    $("[data-hero='user.profile.name").text(user.name.first + " " + user.name.last);
+    $("[data-hero='user.profile.firstname").text(user.name.first);
+  }
+
+   ///Used to work out where we are in the text
+ var locationIndex = 0;
+ var custData = {
+   "_id": 234324342,
+   "name": "Dean Meehan",
+   "email": "d3an.meehan@hotmail.com",
+   "order": [{
+       "_id": 2423423,
+       "product": "Samsung 2006 4K Laptop",
+       "description": "laptop",
+       "image": "https://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/4/2/42/alu/42-alu-space-sport-black-s1-1up?wid=470&hei=556&fmt=png-alpha&.v=1512435115164",
+       "reviews": ["", "", "", "", "", "", "", "", "", "", ""]
+     },
+     {
+       "_id": 2423423,
+       "product": "Sony QLED 2017 AE3000B32",
+       "description": "tv cable screen stand off",
+       "image":"https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5947/5947110_sd.jpg;maxHeight=1000;maxWidth=1000",
+       "reviews": ["", "", "", "", "", "", "", "", "", "", ""]
+     },
+     {
+       "_id": 2423423,
+       "product": "Apple Watch 2018",
+       "description": "watch battery strap",
+       "image":"https://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/4/2/42/alu/42-alu-space-sport-black-s1-1up?wid=470&hei=556&fmt=png-alpha&.v=1512435115164",
+       "reviews": ["", "", "", "", "", "", "", "", "", "", ""]
+     }
+   ]
+ };
+
+ function scrollDivToBottom(){
+    document.getElementById("chatWindow").scrollTop = document.getElementById("chatWindow").scrollHeight
+ }
+
+  //When the page is loaded this stops the code updating after
+  function loaded() {
+    $('[data-hero-page]').hide();
+    if (csr != null && user != null) {
+      $('#main-content').show();
+      update();
+      return true;
+    }
+    return false;
+  }
+
+  function contains(str, substring){
+      if(str==undefined || str==""){
+          return;
+      }
+      str = str.toLowerCase();
+      substring = substring.toLowerCase();
+      console.log(str, substring, str.indexOf(substring) !== -1)
+      return str.indexOf(substring) !== -1;
+  }
